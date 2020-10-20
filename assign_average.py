cbfd64f5763d8c48fc5e7d88f298ee628d1a87fa
Btrenary <btrenary25@gmail.com>
2020-10-19 21:38:29 -0500

    3 pass

--- assign_average.py
+++ assign_average.py
@@ -8,6 +8,7 @@ Program: assign_average.py
 def switch_average(entry):
     my_dict = {'A': 'You entered an A!',
                'B': 'You entered a B!',
+               'C': 'You entered a C!'
                }
 
     result = my_dict.get(entry)
