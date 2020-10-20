e192045ca749ed45eb3005740f2725c8e8d0cc54
Btrenary <btrenary25@gmail.com>
2020-10-19 21:31:15 -0500

    all fail

--- assign_average.py
+++ assign_average.py
@@ -1,18 +1,12 @@
 """
-
+Author: Brady Trenary
+Program: assign_average.py
 
 """
 
 
 def switch_average(entry):
-    my_dict = {'A': 'You entered an A!',
-               'B': 'You entered a B!',
-               'C': 'You entered a C!',
-               'D': 'You entered a D!',
-               'F': 'You entered an F!'}
-
-    result = my_dict.get(entry, "This is not a valid Grade")
-    return result
+    pass
 
 
 if __name__ == '__main__':
--- test_assign_average.py
+++ test_assign_average.py
@@ -5,7 +5,22 @@ import assign_average
 
 class MyTestCase(unittest.TestCase):
     def test_A(self):
-        self.assertEqual(True, False)
+        self.assertEqual("You entered an A!", assign_average.switch_average('A'))
+
+    def test_B(self):
+        self.assertEqual("You entered a B!", assign_average.switch_average('B'))
+
+    def test_C(self):
+        self.assertEqual("You entered a C!", assign_average.switch_average('C'))
+
+    def test_D(self):
+        self.assertEqual("You entered a D!", assign_average.switch_average('D'))
+
+    def test_F(self):
+        self.assertEqual("You entered an F!", assign_average.switch_average('F'))
+
+    def test_non_key(self):
+        self.assertEqual("This is not a valid Grade", assign_average.switch_average('R'))
 
 
 if __name__ == '__main__':
